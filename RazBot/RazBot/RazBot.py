9fca8ec1e3b06b414e302bfc8c158c2924872319
nxg9997 <nxg9997@rit.edu>
2018-01-19 00:32:28 -0500

    added nextround

--- RazBot/RazBot/RazBot.py
+++ RazBot/RazBot/RazBot.py
@@ -1,5 +1,6 @@
 # imports
 import discord
+import data
 from discord.ext.commands import Bot
 from discord.ext import commands
 
@@ -7,44 +8,65 @@ client = discord.Client()
 bot_prefix = "!"
 bot = commands.Bot(command_prefix=bot_prefix)
 
+# variables
+
+
+
+# Start Up
 @bot.event
 async def on_ready():
-    print("Hello World")
+    print("RazBot Connected")
 
+# Commands
 @bot.command(pass_context=True)
 async def ping(ctx):
     await bot.say("pong")
 
 @bot.command(pass_context=True)
 async def leave(ctx):
+    print("Disconnected")
     await bot.close()
 
 @bot.command(pass_context=True)
 async def joinV(ctx):
-    channelToJoin = bot.get_channel('273826458928152577') # general channel
-    print(channelToJoin.id)
+    cmd,chan = ctx.message.content.split(' ')
+    channelToJoin = bot.get_channel(chan)
+    print("Joined: " + channelToJoin.name)
     await bot.join_voice_channel(channelToJoin)
 
 @bot.command(pass_context=True)
 async def move(ctx):
-    #toMove = bot.member
     text = ctx.message.content
     cmd,user,chan = text.split(' ')
-    print(user)
-    print(chan)
-    #userInfo = bot.get_user_info(user)
+    # print(user)
+    # print(chan)
     channel = bot.get_channel(chan)
-    #print(userInfo.name)
-    userInfo = ctx.message.mentions[0]#discord.utils.get(ctx.message.server.members, name=user)
-    print(userInfo)
-    await bot.move_member(userInfo, channel) # scrubs channel
+    userInfo = ctx.message.mentions[0]
+    # print(userInfo)
+    print("Moved: " + userInfo.name + " -> " + channel.name)
+    await bot.move_member(userInfo, channel)
 
 @bot.command(pass_context=True)
 async def GetUser(ctx):
     text = ctx.message.content
     cmd,user = text.split(' ')
     print("Entered Name: " + user)
-    member = ctx.message.mentions[0]#discord.utils.get(ctx.server.members, name=user)
+    member = ctx.message.mentions[0]
     print(member)
+
+@bot.command(pass_context=True)
+async def NextRound(ctx):
+    channels = ["",""]
+    for x in range(len(data.userdata)):
+        member,channels[0],channels[1] = data.userdata[x].split(',')
+        # print(member)
+        # print(channels[data.roundNum])
+        mem = discord.utils.get(ctx.message.server.members, name=member)
+        # print(mem)
+        channel = bot.get_channel(channels[data.roundNum])
+        print("Moved: " + member + " -> " + channel.name)
+        await bot.move_member(mem, channel)
+        
+    data.roundNum += 1
 
 bot.run("NDAzNzM3OTQyNTUzNTI2Mjcz.DULqmA.M9aEeIvv34Mj7KvdU2aWMklwCtc")
---
+++ RazBot/RazBot/data.py
@@ -0,0 +1,6 @@
+
+# Data to be used by the discord bot
+
+userdata = ["RazExtra,312812936789753856,273826458928152577"]#9676
+
+roundNum = 0
